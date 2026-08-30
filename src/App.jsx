b73d96c5ff93ac056abf6c42843c7ea78fb8033b
johannesstenmarck <johannesstenmarck@gmail.com>
2026-08-30 14:48:03 +0200

    Add CookieConsent component to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import SiteShell from "./components/SiteShell.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import OmHypnosPage from "./pages/OmHypnosPage.jsx";
 import OmMigPage from "./pages/OmMigPage.jsx";
+import CookieConsent from "./components/CookieConsent.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SiteShell />}>
-        <Route index element={<HomePage />} />
-        <Route path="om-hypnos" element={<OmHypnosPage />} />
-        <Route path="om-mig" element={<OmMigPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<SiteShell />}>
+          <Route index element={<HomePage />} />
+          <Route path="om-hypnos" element={<OmHypnosPage />} />
+          <Route path="om-mig" element={<OmMigPage />} />
+        </Route>
+      </Routes>
+
+      <CookieConsent />
+    </>
   );
 }
